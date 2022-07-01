8079f8e8d8aae7291b3d4da68a6a389d6d9323d2
Richard Hagen <richard.hagen@gmail.com>
2022-07-01 19:53:05 +0200

    renamed cookie auth variable

--- src/cookie.js
+++ src/cookie.js
@@ -1,6 +1,6 @@
 
 export const COOKIE = {
-    cookieName: "zaptec_user",
+    cookieName: "zaptec_auth",
     password: process.env.SESSION_KEY,
     // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
     cookieOptions: {
